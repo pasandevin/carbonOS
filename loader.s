0d0534cd74f659f9aab772f8e27c148968dd85a5
pasandevin <jayawardene.pasandevin@gmail.com>
2021-07-21 11:46:41 +0530

    set up stack pointer

--- loader.s
+++ loader.s
@@ -18,7 +18,8 @@
         dd FLAGS                    ; the flags,
         dd CHECKSUM                 ; and the checksum
 
-    loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+    loader:                         			; the loader label (defined as entry point in linker script)
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   	; point esp to the start of the
+                                                	; stack (end of memory area)
     .loop:
         jmp .loop                   ; loop forever
