fbb5f3f3234dc083de928a0024c4a1f2bf29df7a
Pasan Devin Jayawardene <60750424+pasandevin@users.noreply.github.com>
2021-07-21 12:34:59 +0530

    Implement with c (#2)
    * stack set up
    
    * set up stack pointer
    
    * deleted unnecessary files
    
    * added kmain.c
    
    * called c function from assembly
    
    * configured the makefile
    
    * modified makefile and loader.s
    
    * added generated files

---
+++ kmain.c
@@ -0,0 +1,5 @@
+    /* The C function */
+    int sum_of_three(int arg1, int arg2, int arg3)
+    {
+        return arg1 + arg2 + arg3;
+    }
--- loader.s
+++ loader.s
@@ -4,6 +4,13 @@
     FLAGS        equ 0x0            ; multiboot flags
     CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
+                                    
+    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+
+    section .bss
+    align 4                         ; align at 4 bytes
+    kernel_stack:                   ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
 
     section .text:                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
@@ -11,7 +18,18 @@
         dd FLAGS                    ; the flags,
         dd CHECKSUM                 ; and the checksum
 
-    loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+    loader:                         			; the loader label (defined as entry point in linker script)
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   	; point esp to the start of the
+                                                	; stack (end of memory area)
+        
+        
+    ; The assembly code
+    extern sum_of_three   		  ; the function sum_of_three is defined elsewhere
+    push dword 3           		  ; arg3
+    push dword 2            	     	  ; arg2
+    push dword 1            		  ; arg1
+    call sum_of_three                   ; call the function, the result will be in eax
+    
+    
     .loop:
         jmp .loop                   ; loop forever
