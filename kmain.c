db23de8d9e67076acbccc50ee884be03fe55faf9
pasandevin <jayawardene.pasandevin@gmail.com>
2021-08-03 17:59:01 +0530

    implemented the function: fb_write_cell

--- kmain.c
+++ kmain.c
@@ -1,9 +1,13 @@
-    /* The C function */
-    int sum_of_three(int arg1, int arg2, int arg3)
+    #define FB_GREEN     2
+    #define FB_DARK_GREY 8
+    char *fb = (char *) 0x000B8000;
+    void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
     {
-        return arg1 + arg2 + arg3;
+        fb[i] = c;
+        fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
     }
     
     int main(){
-    	return sum_of_three(1,2,4);
+
+   	 fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
     }
