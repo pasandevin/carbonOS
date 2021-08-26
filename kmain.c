a88f7d38fe6fab069156c7d2a2c7a3384f031459
pasandevin <jayawardene.pasandevin@gmail.com>
2021-08-26 22:05:31 +0530

    added the multiboot.h header file

--- kmain.c
+++ kmain.c
@@ -1,22 +1,24 @@
-    #include "drivers/framebuffer.h"
-    #include "drivers/serial_port.h"
-    #include "segmentation/memory_segments.h"
-    #include "interrupts/keyboard.h"
-    #include "interrupts/interrupts.h"
+#include "drivers/framebuffer.h"
+#include "drivers/serial_port.h"
+#include "segmentation/memory_segments.h"
+#include "interrupts/keyboard.h"
+#include "interrupts/interrupts.h"
+
+void init_segments_interrupts(){
+	segments_install_gdt();
+	interrupts_install_idt();
+
+}
+
+int kmain(){
+	init_segments_interrupts();
+	char ptr2[] = "Welcome to CarbonOS!     ";
 
 
-    
-    int kmain(){
+	serial_write(0x3F8, ptr2, 25);
+	fb_write(ptr2, 25);
 
-           char ptr2[] = "Welcome to CarbonOS!     ";
-   
-    
-    serial_write(0x3F8, ptr2, 25);
-    fb_write(ptr2, 25);
 
-    segments_install_gdt();
-    interrupts_install_idt();
-    
-    return 0;
-    
-    }
+	return 0;
+
+}
