bec101905052363406a5d6a774a3fe467f7323a0
pasandevin <jayawardene.pasandevin@gmail.com>
2021-09-06 16:36:39 +0530

    init paging

--- kmain.c
+++ kmain.c
@@ -4,20 +4,22 @@
 #include "interrupts/keyboard.h"
 #include "interrupts/interrupts.h"
 #include "user_mode/start_program.h"
+#include "paging.h"
 
 
 /*function to intialize interrupts and segments*/
-void init_segments_interrupts(){
+void init(){
 	segments_install_gdt();
 	interrupts_install_idt();
+	enable_paging();
 
 }
 
 /*kernal main funcion*/
-void kmain(unsigned int ebx){
+void kmain(){
 
-	init_segments_interrupts();   //initialize interrunpts and segments
-	run_custom_program(ebx);      //run the user program
+	init();   //initialize interrunpts and segments
+	//run_custom_program(ebx);      //run the user program
 
 
 }
---
+++ paging.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_PAGING_H
+#define INCLUDE_PAGING_H
+
+void enable_paging(); 
+
+#endif 
+
+
